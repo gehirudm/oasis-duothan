4ce0425865c79b6431bbdbb3bc12de74c30b7ba1
Nimsith79 <nimsith.xyz@gmail.com>
2025-07-16 14:46:49 +0530

    hackathon name changed

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -16,7 +16,7 @@ export function Navbar() {
         <div className="flex justify-between h-16">
           <div className="flex items-center">
             <Link href="/" className="text-xl font-bold text-primary">
-              HackPlatform
+              OASIS-CTF
             </Link>
           </div>
 
